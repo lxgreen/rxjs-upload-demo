03792bb6733da54732d4abe1ed8e866c097b9687
Alex Greenstein <lxgreen@users.noreply.github.com>
2022-02-06 01:51:27 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,19 @@ import LogViewer from "./components/LogViewer";
 import Toaster from "./components/Toaster";
 import ImageViewer from "./components/ImageViewer";
 import { ServiceContext } from "./context/service-context";
-import { LogService, NotificationService } from "./models";
 import "./styles.css";
-
-const logTestMessage = (logger: LogService) => (message: string) =>
-  logger.log(message);
-const resetLog = (logger: LogService) => () => logger.reset();
-
-const addInfo = (notifier: NotificationService) => (notification: string) =>
-  notifier.info(notification);
-
-const addError = (notifier: NotificationService) => (notification: string) =>
-  notifier.error(notification);
-
-const resetNotifier = (notifier: NotificationService) => () =>
-  notifier.removeAll();
 
 export default function App(): FC {
   const { logger, notifier, uploader } = useContext(ServiceContext);
   let counter = 0;
+
+  const logTestMessage = (message: string) => logger.log(message);
+  const resetLog = () => logger.reset();
+  const addInfo = (notification: string) => notifier.info(notification);
+  const addError = (notification: string) => notifier.error(notification);
+  const resetNotifier = () => notifier.removeAll();
+  const upload = () => uploader.selectFiles();
+  const removeImages = () => uploader.removeAll();
 
   return (
     <div className="App">
@@ -32,37 +26,28 @@ export default function App(): FC {
         <Toaster />
       </div>
       <div className="footer">
-        <button
-          key="log"
-          onClick={() => logTestMessage(logger)(`TEST ${++counter}`)}
-        >
+        <button key="log" onClick={() => logTestMessage(`TEST ${++counter}`)}>
           LOG TEST MESSAGE
         </button>
-        <button key="resetLog" onClick={resetLog(logger)}>
+        <button key="resetLog" onClick={resetLog}>
           CLEAR LOG
         </button>
-        <button
-          key="info"
-          onClick={() => addInfo(notifier)(`GOOD ${++counter}`)}
-        >
+        <button key="info" onClick={() => addInfo(`GOOD ${++counter}`)}>
           INFO
         </button>
-        <button
-          key="error"
-          onClick={() => addError(notifier)(`ERROR ${++counter}`)}
-        >
+        <button key="error" onClick={() => addError(`ERROR ${++counter}`)}>
           ERROR
         </button>
-        <button key="resetNotify" onClick={resetNotifier(notifier)}>
+        <button key="resetNotify" onClick={resetNotifier}>
           CLEAR NOTIFICATIONS
         </button>
-        <button key="uploadImage" onClick={() => uploader.selectFiles()}>
+        <button key="uploadImage" onClick={upload}>
           UPLOAD IMAGES
         </button>
-        <button key="cancelUploads" onClick={() => uploader.removeAll()}>
+        <button key="cancelUploads" onClick={removeImages}>
           REMOVE IMAGES
         </button>
-        <button key="failUploadImage">NOOP</button>
+        <button key="noop">NOOP</button>
       </div>
       <div className="logger">
         <LogViewer />
